55815749547a3b42da82d26bf77e930ba88dabe9
Q. Price <priceqp@gmail.com>
2019-07-29 23:15:48 -0500

    Add icons and conditional linethrough to Todo component

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 function Todo({ completed, task }) {
   return (
     <ListItem>
       <Checkbox tabIndex={-1} checked={completed} />
-      <ListItemText>{task}</ListItemText>
+      <ListItemText
+        style={{ textDecoration: completed ? 'line-through' : 'none' }}
+      >
+        {task}
+      </ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label='Delete'>
+          <DeleteIcon />
+        </IconButton>
+        <IconButton aria-label='Edit'>
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
