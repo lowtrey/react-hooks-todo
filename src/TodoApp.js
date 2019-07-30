b74d6fdf268694498ae25d710280d5c6e90f3a3d
Q. Price <priceqp@gmail.com>
2019-07-30 17:44:46 -0500

    Create useTodoState hook and move todo methods over to it

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -6,42 +6,21 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import uuid from 'uuid/v4';
+import useTodoState from './hooks/useTodoState';
 
 function TodoApp() {
   const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]');
-  // const initialTodos = [
-  //   { id: 1, task: 'Clean Fishtank', completed: false },
-  //   { id: 2, task: ' Wash Car', completed: true },
-  //   { id: 3, task: 'Grow Beard', completed: false }
-  // ];
-  const [todos, setTodos] = useState(initialTodos);
+  const { 
+    addTodo, 
+    editTodo, 
+    removeTodo, 
+    todos, 
+    toggleTodo 
+  } = useTodoState(initialTodos);
 
   useEffect(() => {
     window.localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
-
-  const addTodo = newTodoText => {
-    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
-  };
-  const removeTodo = todoId => {
-    // filter out removed todo
-    const updatedTodos = todos.filter(todo => todo.id !== todoId);
-    // call setTodos with new todos array
-    setTodos(updatedTodos);
-  };
-  const toggleTodo = todoId => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
-  };
-  const editTodo = (todoId, newTask) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === todoId ? { ...todo, task: newTask } : todo
-    );
-    setTodos(updatedTodos);
-  };
 
   return (
     <Paper 
