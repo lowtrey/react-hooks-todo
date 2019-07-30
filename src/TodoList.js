bc8b51793a3b4fa32491b77327bd2ac1503c0c8c
Q. Price <priceqp@gmail.com>
2019-07-30 10:16:37 -0500

    Render TodoList only if todos has a length

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,26 +5,28 @@ import Paper from '@material-ui/core/Paper';
 import Todo from './Todo';
 
 function TodoList({ editTodo, removeTodo, todos, toggleTodo }) {
-  return (
-    <Paper>
-      <List>
-        {todos.map((todo, i) => (
-          <>
-            <Todo 
-              id={todo.id}
-              task={todo.task} 
-              key={todo.id} 
-              completed={todo.completed} 
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              editTodo={editTodo}
-            />
-            {i < todos.length - 1 && <Divider />}
-          </>
-        ))}
-      </List>
-    </Paper>
-  );
+  if(todos.length)
+    return (
+      <Paper>
+        <List>
+          {todos.map((todo, i) => (
+            <>
+              <Todo 
+                id={todo.id}
+                task={todo.task} 
+                key={todo.id} 
+                completed={todo.completed} 
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                editTodo={editTodo}
+              />
+              {i < todos.length - 1 && <Divider />}
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  return null;
 }
 
 export default TodoList;
