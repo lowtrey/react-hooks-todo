608187a6b268a50794db08907cfc54f9bdb17eba
Q. Price <priceqp@gmail.com>
2019-07-29 21:01:37 -0500

    Continue constructing TodoList component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
 
 function TodoList(props) {
   return (
-    <h1>TodoList</h1>
+    <Paper>
+      <List>
+        {props.todos.map(todo => (
+          <>
+            <ListItem>
+              <ListItemText>{todo.task}</ListItemText>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
   );
 }
 
